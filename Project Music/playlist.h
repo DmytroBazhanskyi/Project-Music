023aa5a00837da800d8abd2e031185cae895aacf
KuryloValeria <huawei.y3.huawei.user@gmail.com>
2025-03-22 23:56:26 +0200

    playlist.h

--- Project Music/playlist.h
+++ Project Music/playlist.h
@@ -1,1 +1,16 @@
+#include "track.h"
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
+
+class Playlist {
+private:
+    std::vector<Track> tracks;
+
+public:
+    void addTrack(const Track& track);  // Додати трек вручну
+    void addTrackFromFile(const std::string& filename);  // Додати треки з файлу
+    void printPlaylist() const;  // Вивести плейлист
+    int getTotalDurationByGenre(const std::string& genre) const;  // Підрахувати загальну тривалість для жанру
 
